063a761a511aa53049501fa2bf6b18ab672e0fa3
Abdelrhman Atef <73834147+abdelrhmanatef87@users.noreply.github.com>
2023-10-21 16:59:06 +0300

    Update Plateform_Types.h

--- Unit_3_Embedded_C/Lesson 1/Assignment/Platform_Types/Plateform_Types.h
+++ Unit_3_Embedded_C/Lesson 1/Assignment/Platform_Types/Plateform_Types.h
@@ -6,23 +6,23 @@
 
 typedef unsigned char           Uint8_t;
 typedef char                    int8_t;
-typedef unsigned short 			Uint16_t;
-typedef short 					int16_t;
-typedef unsigned int 			Uint32_t;
-typedef int 					int32_t;
-typedef unsigned long long 		Uint64_t;
-typedef long long 				int64_t;
+typedef unsigned short 			    Uint16_t;
+typedef short 					        int16_t;
+typedef unsigned int 			      Uint32_t;
+typedef int 					          int32_t;
+typedef unsigned long long 		  Uint64_t;
+typedef long long 				      int64_t;
 
 //for signed and  unsigned ,  volatile
 
-typedef volatile unsigned char 			Vuint8_t;
-typedef volatile char 					Vint8_t;
-typedef volatile unsigned short		    Vuint16_t;
-typedef volatile short 					Vint16_t;
-typedef volatile unsigned int 			Vuint32_t;
-typedef volatile int 					Vint32_t;
-typedef volatile unsigned long long 	Vuint64_t;
-typedef volatile long long 				Vint64_t;
+typedef volatile unsigned char 			 Vuint8_t;
+typedef volatile char 					     Vint8_t;
+typedef volatile unsigned short		   Vuint16_t;
+typedef volatile short 					     Vint16_t;
+typedef volatile unsigned int 			 Vuint32_t;
+typedef volatile int 					       Vint32_t;
+typedef volatile unsigned long long  Vuint64_t;
+typedef volatile long long 				   Vint64_t;
 
 //for character
 typedef char 	 	char_t;
